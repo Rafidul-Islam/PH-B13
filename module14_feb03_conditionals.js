50479679687581b5e9a52e4b9823aa87da7a4563
Rafid <rafid.iconic@gmail.com>
2026-02-03 22:05:57 +0600

    not interesting anymore

---
+++ module14_feb03_conditionals.js
@@ -0,0 +1,29 @@
+// console.log(10 > 5);
+
+let age = 17;
+
+// if (age > 18) {
+//   console.log("Voter");
+// } else {
+//   console.log("Non-Voter");
+// }
+
+// ==  ---> Only Value
+// === ---> Value + Type
+// !=  ---> Not Equal
+
+// let cond = age > 18 ? "voter" : "non-voter";
+// console.log(cond);
+
+const isPassed = false;
+const num = 86;
+
+if (!isPassed) {
+  console.log("Pass");
+
+  if (num > 80) {
+    console.log("Grade : A+");
+  }
+} else {
+  console.log("fail");
+}
